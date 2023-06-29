8a70157c7fbd901694cdb67fa64ce6b8c5ca3a05
Oleg <olegbilas@gmail.com>
2023-06-29 17:27:56 +0300

    Update Loader.jsx

--- components/Loader.jsx
+++ components/Loader.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { RotatingLines } from "react-loader-spinner";
+import { View } from "react-native";
 
 export default function Loader() {
   return (
-    <div className={css.loader}>
+    <View>
       <RotatingLines
         strokeColor="grey"
         strokeWidth="4"
         animationDuration="0.75"
-        width="96"
+        width="46"
         visible={true}
       />
-    </div>
+    </View>
   );
 }
